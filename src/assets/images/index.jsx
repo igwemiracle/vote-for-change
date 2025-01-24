00b68d5070e13a2571940b5a32bcc2653f5d76a6
igwemiracle <igwemiracle35@gmail.com>
2025-01-24 08:32:22 +0100

    vote-change

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,9 +1,8 @@
 import tini from './tini.png'
 import buhari from './buhari.png'
 import jona from './jona.png'
-import voting1 from './voting1.png'
-import voting2 from './voting2.png'
+import voting from './voting.png'
 
 export const images = {
-  tini, buhari, jona, voting1, voting2
+  tini, buhari, jona, voting
 }
