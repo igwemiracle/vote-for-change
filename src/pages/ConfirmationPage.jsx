39a42aed737a041a6aefe6182ef454a901ed98c0
igwemiracle <igwemiracle35@gmail.com>
2025-01-30 16:19:54 +0100

    vote-change

--- src/pages/ConfirmationPage.jsx
+++ src/pages/ConfirmationPage.jsx
@@ -4,7 +4,7 @@ import { AiOutlineCheckCircle } from 'react-icons/ai'; // Importing a success ic
 const ConfirmationPage = () => {
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-80 sm:w-96 md:w-[500px] lg:w-[600px] text-center">
+      <div className="flex flex-col items-center bg-white p-6 rounded-lg shadow-lg w-80 sm:w-96 md:w-[500px] lg:w-[600px] text-center">
         <AiOutlineCheckCircle className="text-green-500 text-6xl mb-4" />
         <h2 className="text-3xl font-bold text-gray-900 mb-4">Thank You!</h2>
         <p className="text-lg text-gray-700">Your vote has been successfully cast.</p>
